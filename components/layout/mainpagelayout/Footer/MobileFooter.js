2dab760b5d3c48bcd40ec060c7efd534e679e961
saadhajjouji <skyeayyad@gmail.com>
2022-10-17 13:22:56 +0100

    stuff

--- components/layout/mainpagelayout/Footer/MobileFooter.js
+++ components/layout/mainpagelayout/Footer/MobileFooter.js
@@ -1,18 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import classes from "./MobileFooter.module.scss";
 const MobileFooter = () => {
   const FooterContent = [
     { title: "News", sections: ["Home Page", "World", "CoronaVirus"] },
     { title: "Arts", sections: ["Today's Arts", "Art & Design", "Books"] },
     { title: "Living", sections: ["Automotive", "Games", "Education"] },
-    { title: "Living", sections: ["Automotive", "Games", "Education"] },
+    { title: "live", sections: ["Automoti", "Game", "Educati"] },
   ];
+  const [element, setElement] = useState();
+  const showElement = (title) => {
+    if (element === title) {
+      setElement("");
+    } else setElement(title);
+  };
   return (
     <>
       {FooterContent.map((content) => (
-        <div key={content.title}>
-          <h3>{content.title}</h3>
-          <ul>
+        <div className={classes.MobileFooter} key={content.title}>
+          <h3 onClick={() => showElement(content.title)}>{content.title}</h3>
+          <ul
+            className={
+              element === content.title
+                ? classes.normalHeight
+                : classes.zeroHeight
+            }
+          >
             {content.sections.map((section) => (
               <li key={section[0]}>{section}</li>
             ))}
