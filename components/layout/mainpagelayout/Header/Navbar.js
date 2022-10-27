501528bd925ed35e983a2ba81a4aa9a596b074ff
saadhajjouji <skyeayyad@gmail.com>
2022-10-27 14:18:06 +0100

    stuff

--- components/layout/mainpagelayout/Header/Navbar.js
+++ components/layout/mainpagelayout/Header/Navbar.js
@@ -2,15 +2,34 @@ import Link from "next/link";
 import React from "react";
 import classes from "./Navbar.module.scss";
 const Navbar = () => {
+  const categories = [
+    "world",
+    "politics",
+    "business",
+    "opinion",
+    "tech",
+    "science",
+    "health",
+    "sports",
+    "arts",
+    "books",
+    "food",
+    "travel",
+  ];
   return (
-    <div className={classes.desktopNavBar}>
-      <ul>
-        <li>
-          <Link href={`/${1}`}>news</Link>
-        </li>
-       
-      </ul>
-    </div>
+    <>
+      <div className={classes.desktopNavBar}>
+        <ul>
+          {categories.map((category) => (
+            <li key={category}>
+              <Link href={`/${1}`}>{category}</Link>
+            </li>
+          ))}
+        </ul>
+        <hr className={`${classes.line} ${classes.line1}`}/>
+        <hr className={classes.line} />
+      </div>
+    </>
   );
 };
 
