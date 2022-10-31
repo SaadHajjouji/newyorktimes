ff6c74a12d07a7b2af6ff5ac910cdafb31e4dcd6
saadhajjouji <skyeayyad@gmail.com>
2022-10-31 18:04:04 +0100

    right side main

--- components/mainPage/MainPage.js
+++ components/mainPage/MainPage.js
@@ -5,16 +5,26 @@ import classes from "./Main.module.scss";
 const MainPage = ({ content }) => {
   const globalData = content.results;
   const opinionData = globalData.filter((data) => data.section === "opinion");
-  const artsData = globalData.filter((data) => data.section === "arts");
+  const EntertainmentData = globalData
+    .filter(
+      (data) =>
+        data.section === "arts" ||
+        data.section === "style" ||
+        data.section === "sports"
+    )
+    .filter((el, i) => i < 3);
   const worldDataSection = globalData.filter(
     (data) => data.section === "world"
   )[0];
   const worldData = globalData.filter((data) => data.section === "world");
   const usData = globalData.filter((data) => data.section === "us");
   const elections = globalData
-    .filter((data) => data.subsection === "elections")
-    .filter((el, i) => i < 2);
-  console.log(elections);
+    .filter(
+      (data) =>
+        data.subsection === "elections" || data.subsection === "politics"
+    )
+    .filter((el, i) => i <= 1);
+
   const featuredWorld = {
     title: worldDataSection.title,
     abstract: worldDataSection.abstract,
@@ -25,6 +35,40 @@ const MainPage = ({ content }) => {
     url: worldDataSection.url,
     date: worldDataSection.published_date,
   };
+  const featEntertainment = [
+    {
+      title: EntertainmentData[0].title,
+      abstract: EntertainmentData[0].abstract,
+      image: EntertainmentData[0].multimedia[1].url,
+      width: EntertainmentData[0].multimedia[1].width,
+      height: EntertainmentData[0].multimedia[1].height,
+      caption: EntertainmentData[0].multimedia[1].caption,
+      url: EntertainmentData[0].url,
+      date: EntertainmentData[0].published_date,
+    },
+    {
+      title: EntertainmentData[1].title,
+      abstract: EntertainmentData[1].abstract,
+      image: EntertainmentData[1].multimedia[1].url,
+      width: EntertainmentData[1].multimedia[1].width,
+      height: EntertainmentData[1].multimedia[1].height,
+      caption: EntertainmentData[1].multimedia[1].caption,
+      url: EntertainmentData[1].url,
+      date: EntertainmentData[1].published_date,
+    },
+    {
+      title: EntertainmentData[2].title,
+      abstract: EntertainmentData[2].abstract,
+      image: EntertainmentData[2].multimedia[1].url,
+      width: EntertainmentData[2].multimedia[1].width,
+      height: EntertainmentData[2].multimedia[1].height,
+      caption: EntertainmentData[2].multimedia[1].caption,
+      url: EntertainmentData[2].url,
+      date: EntertainmentData[2].published_date,
+    },
+  ];
+
+  console.log(globalData);
   return (
     <div>
       <div className={classes.generalLayout}>
@@ -50,7 +94,22 @@ const MainPage = ({ content }) => {
             </div>
           </article>
         </div>
-        <div className={classes.rightSection}>ll</div>
+        <div className={classes.rightSection}>
+          <div className={classes.EntertainmentSection}>
+            <div className={classes.section1}>
+              <ImageCaption
+                featuredImg={featEntertainment[0].image}
+                caption={featEntertainment[0].caption}
+                width={featEntertainment[0].width}
+                height={featEntertainment[0].height}
+              />
+              <h3>{featEntertainment[0].title}</h3>
+              <p>{featEntertainment[0].abstract}</p>
+            </div>
+            <div className={classes.section2}>no</div>
+            <div className={classes.section3}>bye</div>
+          </div>
+        </div>
       </div>
     </div>
   );
