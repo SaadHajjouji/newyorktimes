5d1fb52797d91ccf4a83b7316a97d08bbd6d6b29
saadhajjouji <skyeayyad@gmail.com>
2022-11-21 10:29:29 +0100

    stuff

--- components/layout/Footer/MobileFooter.js
+++ components/layout/Footer/MobileFooter.js
@@ -1,19 +1,21 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./MobileFooter.module.scss";
+import getTitles from "../../../helpers/dataCenter";
+
 const MobileFooter = () => {
-  const FooterContent = [
-    { title: "News", sections: ["Home Page", "World", "CoronaVirus"] },
-    { title: "Arts", sections: ["Today's Arts", "Art & Design", "Books"] },
-    { title: "Living", sections: ["Automotive", "Games", "Education"] },
-    { title: "live", sections: ["Automoti", "Game", "Educati"] },
-  ];
-  const [element, setElement] = useState();
-  const showElement = (title) => {
-    if (element === title) {
-      setElement("");
-    } else setElement(title);
-  };
+  const [sections, setSections] = useState("");
+
+  useEffect(() => {
+    getTitles()
+      .then((data) => {
+        setSections(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className={classes.mobileFooterWrapper}>
       <Image
@@ -22,22 +24,12 @@ const MobileFooter = () => {
         width={200}
         height={28.5714}
       />
-      {FooterContent.map((content) => (
-        <div className={classes.MobileFooter} key={content.title}>
-          <h3 onClick={() => showElement(content.title)}>{content.title}</h3>
-          <ul
-            className={
-              element === content.title
-                ? classes.normalHeight
-                : classes.zeroHeight
-            }
-          >
-            {content.sections.map((section) => (
-              <li key={section[0]}>{section}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+
+      <div className={classes.MobileFooter}>
+        <ul>
+          <li>hello</li>
+        </ul>
+      </div>
     </div>
   );
 };
